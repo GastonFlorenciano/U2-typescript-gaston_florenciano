28996b99b60d8974916cbe6d778c4591c95ffa20
Gastón Florenciano <gaston__florenciano@hotmail.com>
2025-08-21 13:42:31 -0300

    chore: :sparkles: Hacer ejercicio 11.

---
+++ ejercicio11.ts
@@ -0,0 +1,16 @@
+interface Shape {
+  area(): number
+}
+
+class Circle implements Shape {
+  radio: number
+  constructor(radio: number) {
+    this.radio = radio
+  }
+  area(): number {
+    return Math.PI * this.radio * this.radio
+  }
+}
+
+const circle = new Circle(10)
+console.log(circle.area())
