2c4e81c8133e55c7ebd27d83dc08f23497c9daeb
Gastón Florenciano <gaston__florenciano@hotmail.com>
2025-08-21 12:10:32 -0300

    chore: :sparkles: Hacer ejercicio 2.

---
+++ dist/ejercicio2.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let status = ["activo", 1];
+console.log(`El usuario está ${status[0]} y su ID es ${status[1]}.`);
+//# sourceMappingURL=ejercicio2.js.map
---
+++ ejercicio2.ts
@@ -0,0 +1,3 @@
+let status: [string, number] = ["activo", 1];
+
+console.log(`El usuario está ${status[0]} y su ID es ${status[1]}.`);
