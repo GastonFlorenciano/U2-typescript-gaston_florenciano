dcbd0503936d00f07c16e35bc46c82dcd8e4b814
Gastón Florenciano <gaston__florenciano@hotmail.com>
2025-08-21 12:06:44 -0300

    chore: :sparkles: Hacer ejercicio 1.

---
+++ dist/ejercicio1.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let age = 25;
+let nombre = "Juan";
+console.log(`Mi nombre es ${nombre} y tengo ${age} años.`);
+//# sourceMappingURL=ejercicio1.js.map
---
+++ ejercicio1.ts
@@ -0,0 +1,4 @@
+let age: number = 25;
+let nombre: string = "Juan";
+
+console.log(`Mi nombre es ${nombre} y tengo ${age} años.`);
