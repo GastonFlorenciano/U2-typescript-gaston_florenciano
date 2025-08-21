4cfd350fabcd468bef6540fe6481242b407f0e51
Gastón Florenciano <gaston__florenciano@hotmail.com>
2025-08-21 13:43:46 -0300

    chore: :sparkles: Hacer ejercicio 13.

---
+++ ejercicio13.ts
@@ -0,0 +1,11 @@
+enum Color {
+  Red = 'Red',
+  Green = 'Green',
+  Blue = 'Blue',
+}
+
+function getColor(color: Color): string {
+  return `The color is ${color as Color}`
+}
+
+console.log(getColor(Color.Red))
